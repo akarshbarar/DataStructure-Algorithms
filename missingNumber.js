04e8d47789a4e75b08c70d77d84344a59106730e
akarshbarar <barar.akarsh8@gmail.com>
2022-05-29 00:54:29 +0530

    missing number method 2

--- missingNumber.js
+++ missingNumber.js
@@ -3,13 +3,24 @@
 // Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 
 //! Method One
-var missingNumber = function(nums) {
-    nums = nums.sort();
-    let l = nums.length;
-    if(nums[l-1] != l) return l;
-    for(let i=0;i<l;i++){
-        if(nums[i]-i != 0) {
-            return i;
-        }
+var missingNumber = function (nums) {
+  nums = nums.sort();
+  let l = nums.length;
+  if (nums[l - 1] != l) return l;
+  for (let i = 0; i < l; i++) {
+    if (nums[i] - i != 0) {
+      return i;
     }
+  }
 };
+
+//! Method 2
+let nums = [3, 0, 1];
+let res = nums.length;
+let sum = 0;
+sum = (res * (res + 1)) / 2;
+for (const i in nums) {
+  const element = nums[i];
+  sum -= element;
+}
+console.log(sum);
