506959b3ebc03fe11b4de541fe09674591795359
akarshbarar <barar.akarsh8@gmail.com>
2023-02-01 23:28:07 +0530

    GCD of strings

--- solution.cpp
+++ solution.cpp
@@ -62,21 +62,58 @@ int main() {
     int t; cin>>t;
     
     while(t--) {
-        int n,k;
-        cin>>n>>k;
+        int n; cin>>n;
 
-        vector<int> arr;
-        for (int i = 0; i < n; ++i) {
-            /* code */
+        vector<int> nums;
+        for(int i=0; i<n; i++) {
             int x;
             cin>>x;
-            arr.push_back(x);
+            nums.push_back(x);
         }
 
-        cout<<firstOcc(arr,k)<<endl;
-        cout<<lastOCc(arr,k)<<endl;
+        int l = nums.size();
+        vector<int> left(l,0);
+        vector<int> right(l,0);
 
-        
+        left[0] = nums[0];
+        right[l-1] = nums[l-1];
+
+        for(int i=1; i< l ; i++) {
+            left[i] = left[i-1]+nums[i];
+        }
+        for(int i=l-2; i>=0; i--) {
+            right[i] = right[i+1] + nums[i];
+        }
+
+
+        for(int i=0; i<l; i++){
+            cout<<left[i]<<", ";
+        }
+        cout<<endl;
+        for(int i=0; i<l; i++){
+            cout<<right[i]<<", ";
+        }
+        cout<<endl;
+
+
+        int s = 0;
+        int e = l-1;
+
+        while(s <= e) {
+            int m = s +(e-s)/2;
+            if(left[m-1] == right[m+1]){
+                cout<<m<<endl;
+                break;
+                // return m;
+            }
+            else if (right[m] > left[m]){
+                s = m+1;
+            }
+            else {
+                e = m-1;
+            }
+        }
+
     }
     return 0;
 }
