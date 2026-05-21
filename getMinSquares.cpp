da6b50f3189969276ff29bfed89cc4fc79ce74d3
akarshbarar <barar.akarsh8@gmail.com>
2026-05-22 00:39:57 +0530

    Get min squares

---
+++ getMinSquares.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    int solveByRecursion(int n) {
+        if(n==0) return 0;
+        
+        int ans = n;
+        for(int i=1; i*i<=n; i++) {
+            ans = min(ans, 1+ solveByRecursion(n-i*i));
+        }
+        return ans;
+    }
+    
+    int solveByMemoization(int n, vector<int> &dp) {
+        if(n==0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int ans = n;
+        for(int i=1; i*i<=n; i++) {
+            ans = min(ans, 1+ solveByMemoization(n-i*i, dp));
+        }
+        dp[n] = ans;
+        return dp[n];
+    }
+    
+    int solveByTabulation(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j*j <= n; j++) {
+                int temp = j*j;
+                if(i-temp >= 0)
+                    dp[i] = min(dp[i], 1+ dp[i-temp]);
+            }
+        }
+        return dp[n];
+
+    }
+    int minSquares(int n) {
+        // Code here
+        // return solveByRecursion(n);
+        
+        // vector<int> dp(n+1, -1);
+        // return solveByMemoization(n, dp);
+        
+        return solveByTabulation(n);
+    }
+};
