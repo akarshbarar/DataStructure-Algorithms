9ecd9598db791c4da29b888e46ca563a52b8cff5
akarshbarar <barar.akarsh8@gmail.com>
2026-05-30 15:48:33 +0530

    Check for Survival in an island

---
+++ checkForSurvivalInAnIsland.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int minimumDays(int S, int N, int M) {
+        // code here
+        if(M > N) return -1;
+        if(S > 6 & (N*6) < (M*7) ) return -1;
+        int sunday = S/7;
+        int buyingDays = S - sunday;
+        
+        int totalFood = S*M;
+        int ans = 0;
+        if (totalFood%N == 0){
+            ans =  totalFood/N;
+        }
+        else {
+             ans =  totalFood/N + 1;
+        }
+        if(ans <= buyingDays){
+            return ans;
+        }else {
+            return -1;
+        }
+        
+    }
+};
